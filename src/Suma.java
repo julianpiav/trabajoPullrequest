e2654e20e4283fb540ba86af2727592336f4faa1
JulianPinilla03 <julianpiav@unisabana.edu.co>
2023-03-13 11:48:18 -0500

    agrega numero 3 en la clase suma

--- src/Suma.java
+++ src/Suma.java
@@ -1,6 +1,6 @@
 public class Suma {
-    public int Suma(int numero1, int numero2){
-        System.out.print(numero1+numero2);
+    public int Suma(int numero1, int numero2, int numero3){
+        System.out.print(numero1+numero2+numero3);
         return numero1+numero2;
     }
 }
