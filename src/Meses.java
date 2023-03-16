4878f97cc10dccf9be05b14df18313d5e676d52f
AndreyCG <andreycogo@unisabana.edu.co>
2023-03-15 20:50:37 -0500

    creacion de la clase Meses

---
+++ src/Meses.java
@@ -0,0 +1,58 @@
+public class Meses {
+
+
+        public void mes(int nMes) {
+
+            if (nMes==1){
+                System.out.print("el mes en que estas es: Enero");
+            }
+            else {
+                if(nMes==2){
+                    System.out.print("el mes en que estas es: Febrero");
+                }
+                else {
+                    if(nMes==3){
+                        System.out.print("el mes en que estas es: Marzo");
+                    }
+                    else {
+                        if (nMes == 4) {
+                            System.out.print("el mes en que estas es: Abril");
+                        } else {
+                            if (nMes == 5) {
+                                System.out.print("el mes en que estas es: Mayo");
+                            }
+                            else{
+                                if (nMes==6){
+                                    System.out.print("el mes en que estas es: Junio ");
+                                }else{
+                                    if (nMes==7){
+                                        System.out.print("el mes en que estas es: Julio");
+                                    }else{
+                                        if(nMes==8){
+                                            System.out.print("el mes en que estas es: Agosto");
+                                        }else{
+                                            if (nMes==9){
+                                                System.out.print("el mes en que estas es: Septiembre");
+                                            }else{
+                                                if (nMes==10){
+                                                    System.out.print("el mes en que estas es: Octubre");
+                                                }else{
+                                                    if(nMes==11){
+                                                        System.out.print("el mes en que estas es: Noviembre ");
+                                                    }else{
+                                                        System.out.print("el mes en que estas es: Diciembre");
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+}
